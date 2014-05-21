bafa9f064910a1a9b509a1713f5b27dde0ee9309
Benjamin Martin <hello@ycourse.fr>
2014-05-21 22:03:51 +0200

    hg

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module RailsStripeMembershipSaas
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
 
-    config.force_ssl = false
+    config.force_ssl = true
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
