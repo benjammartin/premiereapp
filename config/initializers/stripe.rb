afe1a4a99154ae5257a8c294b0f25f23d3dca2eb
Benjamin Martin <hello@ycourse.fr>
2014-05-27 13:56:41 +0200

    gg

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,28 @@
 Stripe.api_key = ENV["STRIPE_API_KEY"]
 STRIPE_PUBLIC_KEY = ENV["STRIPE_PUBLIC_KEY"]
 
+
+
 StripeEvent.setup do
+
+	 subscribe 'invoice.payment_failed' do |event|
+    puts "Received Stripe Event 'invoice.payment_failed'"
+    StripeWebhooks.payment_failed(event.data.object.customer)
+  end
+
+
+	subscribe 'invoice.payment_succeeded' do |event|
+    puts "Received Stripe Event 'invoice.payment_succeeded'"
+    StripeWebhooks.payment_succeeded(event.data.object.customer)
+     end
+
+	subscribe 'customer.subscription.created' do |event|
+    puts "Received Stripe Event 'customer.subscription.created'"
+    StripeWebhooks.subscription_created(event.data.object.customer)
+  end
+  
+
+
   subscribe 'customer.subscription.deleted' do |event|
     user = User.find_by_customer_id(event.data.object.customer)
     user.expire
