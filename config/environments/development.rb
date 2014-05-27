574006ef457bc38f71fd80cdb3ea7413372a6deb
Benjamin Martin <hello@ycourse.fr>
2014-05-27 10:47:19 +0200

    ggg

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,15 +23,6 @@ RailsStripeMembershipSaas::Application.configure do
 
 config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "premiereapp.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "premiereapprails@gmail.com",
-    password: ENV["GMAIL_PASSWORD"]
-  }
 
 config.action_mailer.raise_delivery_errors = true
 
