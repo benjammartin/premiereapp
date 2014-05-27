1893f8edfc7648f2c98f7f68078c319107445254
Benjamin Martin <hello@ycourse.fr>
2014-05-27 14:37:49 +0200

    gg

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module RailsStripeMembershipSaas
       g.helper_specs false
     end
 
-    resource '/stripe', :headers => :any, :methods => [:post]
+    
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
