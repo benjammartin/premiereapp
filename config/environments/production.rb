ef829bf24b9a4ec212c602b28f531ac67e4e8750
Benjamin Martin <hello@ycourse.fr>
2014-05-27 12:57:21 +0200

    hh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ RailsStripeMembershipSaas::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'premiereapp.com' }
+  config.action_mailer.default_url_options = { :host => 'example.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
@@ -69,6 +69,15 @@ RailsStripeMembershipSaas::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 
 
 
