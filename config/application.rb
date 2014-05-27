363aa7453156b8a5faaef31c9c77f5784337175e
Benjamin Martin <hello@ycourse.fr>
2014-05-27 14:35:31 +0200

    h

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module RailsStripeMembershipSaas
       g.helper_specs false
     end
 
+    resource '/stripe', :headers => :any, :methods => [:post]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -6,17 +6,17 @@ StripeEvent.setup do
 
 	subscribe 'invoice.payment_failed' do |event|
     puts "Received Stripe Event 'invoice.payment_failed'"
-    StripeWebhooks.payment_failed(event.data.object.customer)
+    user = User.find_by_customer_id(event.data.object.customer)
   end
 
 	subscribe 'invoice.payment_succeeded' do |event|
     puts "Received Stripe Event 'invoice.payment_succeeded'"
-    StripeWebhooks.payment_succeeded(event.data.object.customer)
+    user = User.find_by_customer_id(event.data.object.customer)
      end
 
 	subscribe 'customer.subscription.created' do |event|
     puts "Received Stripe Event 'customer.subscription.created'"
-    StripeWebhooks.subscription_created(event.data.object.customer)
+     user = User.find_by_customer_id(event.data.object.customer)
   end
   
     subscribe 'customer.subscription.deleted' do |event|
